71c1f89471d32cb34609f6414576f311f3bfc672
Dieter Konrad <dk@off217.de>
2018-12-16 12:02:22 +0100

    Ausgabe aller SSID

--- WifiGate/app/src/main/java/it/cutetee/wifigate/MainActivity.kt
+++ WifiGate/app/src/main/java/it/cutetee/wifigate/MainActivity.kt
@@ -5,19 +5,51 @@ import android.net.wifi.WifiManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wifi: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val wifio: WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifi  = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
-        if (wifio.isWifiEnabled){
+        val net = wifi.configuredNetworks // Returns List<WifiConfiguration>
+        // Ausgabe aller irgendwann mal verbundener WLANs SSID
+        net.forEach {
+            Log.d("SSID ", it.SSID)
+        }
+
+
+        // Log.d("SSID ", net[0].SSID)
+
+
+        if (wifi.isWifiEnabled){
             Log.d("WIFIGATE: ", "WLAN ist AKTIVIERT")
+            Toast.makeText(applicationContext, "WLAN Aktiv", Toast.LENGTH_LONG).show()
+
         } else {
             Log.d("WIFIGATE: ", "WLAN ist AUSGESCHALTET")
+            Toast.makeText(applicationContext, "WLAN Ausgeschaltet", Toast.LENGTH_LONG).show()
+
+        }
+
+        val button = findViewById<Button>(R.id.buttonDisconnect)
+        button?.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun getPin() {
+        val pin = findViewById<EditText>(R.id.editTextPin)
+        if (pin != null) {
+            Toast.makeText(this, pin.text, Toast.LENGTH_LONG).show()
         }
     }
 }
